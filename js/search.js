9c82ff22cd26773dd70d2e5061d30e5275467236
RaulButuc <raulbutuc@gmail.com>
2015-03-15 23:08:06 +0000

    Search function in AJAX for the user accounts

---
+++ js/search.js
@@ -0,0 +1,12 @@
+$('#search').keyup(function() {
+    var that = this;
+    $.each($('tr'),function(i, val) {
+        if ($(val).text().indexOf($(that).val()) == -1) {
+            if ($(val).attr('id') != "tableHeader") {
+                $('tr').eq(i).hide();
+            }
+        } else {
+            $('tr').eq(i).show();
+        }
+    });
+});
