8e60273340b2c344191c84f7e34c977a653bb4d1
RaulButuc <raulbutuc@gmail.com>
2015-03-20 17:43:41 +0000

    Updated the main map

--- map.php
+++ map.php
@@ -53,6 +53,48 @@
       #htmlParsingPlaceholder {
         display: none;
       }
+      .controls {
+        border: 1px solid transparent;
+        border-radius: 2px 0 0 2px;
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        height: 32px;
+        outline: none;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+      }
+
+      #pac-input {
+        margin: auto;
+        margin-top: 80px;
+        background-color: #fff;
+        font-family: Roboto;
+        font-size: 15px;
+        font-weight: 300;
+        padding: 0 11px 0 13px;
+        text-overflow: ellipsis;
+        width: 400px;
+        right: 0;
+      }
+
+      #pac-input:focus {
+        border-color: #4d90fe;
+      }
+
+      .pac-container {
+        font-family: Roboto;
+      }
+
+      #type-selector {
+        color: #fff;
+        background-color: #4d90fe;
+        padding: 5px 11px 0px 11px;
+      }
+
+      #type-selector label {
+        font-family: Roboto;
+        font-size: 13px;
+        font-weight: 300;
+      }
     </style>
   </head>
 
@@ -64,8 +106,8 @@
 
     <!-- Map
     ================================================== -->
+    <input id="pac-input" class="controls" type="text" placeholder="Search for a place" <?php if (is_numeric($_GET['lat']) && is_numeric($_GET['long'])) { echo 'value="'.$_GET['lat'].', '.$_GET['long'].'"'; } ?>>
     <div id="fullScreenMap" class="carousel slide" data-ride="carousel"></div>
-    <div id="htmlParsingPlaceholder"></div>
     <!-- /.map -->
 
     <div class="container">
@@ -85,7 +127,7 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/lib/bootstrap.min.js"></script>
     <script src="holder.js"></script>
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.19"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.19&libraries=places"></script>
     <script type="text/javascript" src="js/map_settings.js"></script>
     <script type="text/javascript" src="js/main_map.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
