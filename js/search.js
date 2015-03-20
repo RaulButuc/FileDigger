4272f079be77118fcafaf06e70f752f2a33c13e4
RaulButuc <raulbutuc@gmail.com>
2015-03-20 17:43:10 +0000

    Added a search bar for the map

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
 $('#search').keyup(function() {
     var that = this;
     $.each($('tr'),function(i, val) {
-        if ($(val).text().indexOf($(that).val()) == -1) {
+        if ($(val).find('.name').text().indexOf($(that).val()) == -1) {
             if ($(val).attr('id') != "tableHeader") {
                 $('tr').eq(i).hide();
             }
